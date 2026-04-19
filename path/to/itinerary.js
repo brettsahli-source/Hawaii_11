4bc45948f5e872e784d1a023f6cd5f6704ef4c8a
brettsahli-source <brett.sahli@gmail.com>
2026-04-19 11:50:21 -0500

    Fully integrate Kona Coast and Kauai Zodiac snorkel activities with details, contacts, and links.

---
+++ path/to/itinerary.js
@@ -0,0 +1,25 @@
+// DAY 2 Events
+{
+    name: 'Kona Coast Snorkel Tour',
+    date: '2026-05-01',
+    time: '9:00 AM - 2:30 PM',
+    location: 'Fair Wind Cruises',
+    address: '78-7130 Kaleiopapa Street, Kailua Kona, HI 96740, US',
+    phone: '(808) 322-2788',
+    website: 'https://www.fair-wind.com/',
+    map: 'https://www.google.com/maps/dir/?api=1&destination=78-7130+Kaleiopapa+Street%2CKailua+Kona%2CHI%2C96740%2CUS&destination_place_id=ChIJoaDGMd0FVHkRVRdcKsmYwt0',
+    check_in: 'Check in 15 minutes early',
+    instructions: 'Arrive at the dock by 8:45 AM.'
+},
+
+// DAY 6 Events
+{
+    name: 'Kauai’s SOUTH SIDE Zodiac Boat Snorkel Adventure',
+    date: '2026-05-05',
+    time: '8:00 AM - 11:00 AM',
+    adults: '2',
+    address: '3417 Poipu Road, Koloa, HI 96756, US',
+    check_in: 'Check in 15 minutes early',
+    map: 'https://www.google.com/maps/dir/?api=1&destination=3417+Poipu+Road%2CKoloa%2CHI%2C96756%2CUS&destination_place_id=EiUzNDE3IFBvaXB1IFJkIHN0ZSAxMDUsIEtvbG9hLCBISSwgVVNBIiMaIQoWChQKEgk7gdpP1xoHfBFbbKa_gHjvJxIHc3RlIDEwNQ',
+    instructions: 'Arrive at the location by 7:45 AM.'
+}
